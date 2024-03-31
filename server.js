fad0d9dbe3ccb1eee1bdac1d70a3f2074d2f853a
Lawrence Yi-Hsuan Chuang <green760223@gmail.com>
2024-03-30 18:27:06 -0700

    Update file

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require("express")
 const { createProxyMiddleware } = require("http-proxy-middleware")
+const cors = require("cors") // 引入cors套件
 
 const app = express()
+
+// 允許所有來源
+app.use(
+  cors({
+    origin: true, // 反映請求的Origin值，或者如果是請求沒有包含Origin頭，則設置為'*'
+    credentials: true, // 因為withCredentials設置為true，所以這裡需要設置credentials為true允許發送cookie
+  })
+)
 
 // 配置代理路径
 app.use(
@@ -10,6 +19,10 @@ app.use(
     target: "http://api-driver.marsview.cc/api", // 第三方 API 的 URL
     changeOrigin: true,
     pathRewrite: { "^/api": "" },
+    onProxyRes: function (proxyRes, req, res) {
+      // 移除可能造成CORS問題的不安全頭部
+      delete proxyRes.headers["access-control-allow-origin"]
+    },
   })
 )
 
